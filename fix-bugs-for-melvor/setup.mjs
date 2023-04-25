2b833d331d3fbde64f0aa1f1311cad8010756f79
SaulLawliet <october.sunbathe@gmail.com>
2023-04-25 11:17:33 +0800

    fix-bugs-for-melvor: delete some code

--- fix-bugs-for-melvor/setup.mjs
+++ fix-bugs-for-melvor/setup.mjs
@@ -1,4 +1,16 @@
 export function setup(ctx) {
+
+    // Top-Right Equipment List Show: Suit's color.
+    ctx.onInterfaceReady(async (ctx) => {
+        for (const data of Object.values(equipmentSlotData)) {
+            for (const elem of getEquipmentImageElements(data.id)) {
+                if (elem) {
+                    elem.classList.add('border-combat-outline');
+                }
+            }
+        }
+    });
+
     // 2022-11-02: Fixed by official
     // PalladiumPot: increasedSkillIntervalPercent => decreasedSkillIntervalPercent
     // ctx.patch(Shop, "postDataRegistration").after(function () {
@@ -12,41 +24,32 @@ export function setup(ctx) {
     //     }
     // });
 
-    // Top-Right Equipment List Show: Suit's color.
-    ctx.onInterfaceReady(async (ctx) => {
-        for (const data of Object.values(equipmentSlotData)) {
-            for (const elem of getEquipmentImageElements(data.id)) {
-                if (elem) {
-                    elem.classList.add('border-combat-outline');
-                }
-            }
-        }
-    });
-
+    // 2023-04-25: Fixed by official
     // Load Blueprint Elite Pillar error
-    ctx.patch(Agility, 'replaceCourseWithBlueprint').replace(function(o, blueprint) {
-        const numUnlocked = this.numObstaclesUnlocked;
-        for (let tier = 0; tier < numUnlocked; tier++) {
-            const obstacle = blueprint.obstacles.get(tier);
-            if (obstacle !== undefined) {
-                if (!this.isObstacleBuilt(obstacle))
-                    this.buildObstacle(obstacle);
-            } else {
-                this.destroyObstacle(tier);
-            }
-        }
-        if (blueprint.pillar !== undefined && !this.isPillarBuilt(blueprint.pillar))
-            this.buildPillar(blueprint.pillar);
-        if (blueprint.elitePillar !== undefined && !this.isElitePillarBuilt(blueprint.elitePillar))
-            // this.buildPillar(blueprint.elitePillar);
-            this.buildElitePillar(blueprint.elitePillar); // change this line
-    });
+    // ctx.patch(Agility, 'replaceCourseWithBlueprint').replace(function(o, blueprint) {
+    //     const numUnlocked = this.numObstaclesUnlocked;
+    //     for (let tier = 0; tier < numUnlocked; tier++) {
+    //         const obstacle = blueprint.obstacles.get(tier);
+    //         if (obstacle !== undefined) {
+    //             if (!this.isObstacleBuilt(obstacle))
+    //                 this.buildObstacle(obstacle);
+    //         } else {
+    //             this.destroyObstacle(tier);
+    //         }
+    //     }
+    //     if (blueprint.pillar !== undefined && !this.isPillarBuilt(blueprint.pillar))
+    //         this.buildPillar(blueprint.pillar);
+    //     if (blueprint.elitePillar !== undefined && !this.isElitePillarBuilt(blueprint.elitePillar))
+    //         // this.buildPillar(blueprint.elitePillar);
+    //         this.buildElitePillar(blueprint.elitePillar); // change this line
+    // });
 
+    // 2023-04-25: Fixed by official
     // Mining Gloves: Consumed only by mining Ore
-    ctx.onCharacterLoaded(async (ctx) => {
-        const consumesChargesOn = game.items.getObjectByID('melvorD:Mining_Gloves').consumesChargesOn
-        if (consumesChargesOn && consumesChargesOn.length == 1 && consumesChargesOn[0] instanceof MiningActionEventMatcher && !consumesChargesOn[0].oreTypes) {
-            consumesChargesOn[0].oreTypes = new Set(['Ore']);
-        }
-    });
+    // ctx.onCharacterLoaded(async (ctx) => {
+    //     const consumesChargesOn = game.items.getObjectByID('melvorD:Mining_Gloves').consumesChargesOn
+    //     if (consumesChargesOn && consumesChargesOn.length == 1 && consumesChargesOn[0] instanceof MiningActionEventMatcher && !consumesChargesOn[0].oreTypes) {
+    //         consumesChargesOn[0].oreTypes = new Set(['Ore']);
+    //     }
+    // });
 }
