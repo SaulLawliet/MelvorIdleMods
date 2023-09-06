4dbeb5abb8d0bc61a3ca2272094060fca7ec3a5f
SaulLawliet <october.sunbathe@gmail.com>
2023-09-06 16:31:45 +0800

    another-drop-changes: show drop changes for archaeology

--- another-drop-changes/setup.mjs
+++ another-drop-changes/setup.mjs
@@ -51,4 +51,42 @@ export function setup(ctx) {
         }
     });
 
+    // archaeology
+    ctx.patch(ArtefactDropList, 'setList').replace(function(o, digSite) {
+        const formatWeight = (weight, totalWeight) => {
+            return ` (${(100*weight/totalWeight).toFixed(2)}%)`;
+        }
+
+        digSite.containsDigSiteRequirement
+            ? this.hasItemRequirement.classList.remove("d-none")
+            : this.hasItemRequirement.classList.add("d-none");
+        digSite.artefacts.tiny.sortedDropsArray.map(
+            ({ item, weight, minQuantity, maxQuantity }) => {
+                const el = createElement("div");
+                el.innerHTML =this.getItemDrop(item, maxQuantity, weight) + formatWeight(weight, digSite.artefacts.tiny.totalWeight);
+                this.artefactsTiny.appendChild(el);
+            }
+        );
+        digSite.artefacts.small.sortedDropsArray.map(
+            ({ item, weight, minQuantity, maxQuantity }) => {
+                const el = createElement("div");
+                el.innerHTML = this.getItemDrop(item, maxQuantity, weight) + formatWeight(weight, digSite.artefacts.small.totalWeight);
+                this.artefactsSmall.appendChild(el);
+            }
+        );
+        digSite.artefacts.medium.sortedDropsArray.map(
+            ({ item, weight, minQuantity, maxQuantity }) => {
+                const el = createElement("div");
+                el.innerHTML = this.getItemDrop(item, maxQuantity, weight) + formatWeight(weight, digSite.artefacts.medium.totalWeight);
+                this.artefactsMedium.appendChild(el);
+            }
+        );
+        digSite.artefacts.large.sortedDropsArray.map(
+            ({ item, weight, minQuantity, maxQuantity }) => {
+                const el = createElement("div");
+                el.innerHTML = this.getItemDrop(item, maxQuantity, weight) + formatWeight(weight, digSite.artefacts.large.totalWeight);
+                this.artefactsLarge.appendChild(el);
+            }
+        );
+    });
 }
