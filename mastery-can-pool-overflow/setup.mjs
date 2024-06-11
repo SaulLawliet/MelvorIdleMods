55432fcba0879cec82322d78a4ca6f60a4b9b90b
SaulLawliet <october.sunbathe@gmail.com>
2024-06-11 12:03:25 +0800

    mastery-can-pool-overflow: Fixed for v1.3

--- mastery-can-pool-overflow/setup.mjs
+++ mastery-can-pool-overflow/setup.mjs
@@ -17,51 +17,52 @@ export function setup(ctx) {
         default: true
     });
 
-    ctx.patch(SkillWithMastery, "addMasteryPoolXP").replace(function (o, xp) {
-        // replace old 'masteryPoolCap'
+    ctx.patch(SkillWithMastery, "addMasteryPoolXP").replace(function (o, realm, xp) {
         let increasedMasteryPoolCap = generalSettings.get('increasedMasteryPoolCap');
-        if (increasedMasteryPoolCap < this.game.modifiers.increasedMasteryPoolCap) {
-            increasedMasteryPoolCap = this.game.modifiers.increasedMasteryPoolCap;
+        if (increasedMasteryPoolCap < this.game.modifiers.masteryPoolCap) {
+            increasedMasteryPoolCap = this.game.modifiers.masteryPoolCap;
             generalSettings.set('increasedMasteryPoolCap', increasedMasteryPoolCap);
         }
-        const masteryPoolCap = Math.floor(this.baseMasteryPoolCap * (100 + increasedMasteryPoolCap) / 100);
+        const xpCap = Math.floor((this.getBaseMasteryPoolCap(realm) * (100 + increasedMasteryPoolCap)) / 100);
 
-        const oldBonusLevel = this.getMasteryCheckPointLevel(this._masteryPoolXP);
-        this._masteryPoolXP += xp;
-        this._masteryPoolXP = Math.min(this._masteryPoolXP, masteryPoolCap);
-        this.renderQueue.masteryPool = true;
-        const newBonusLevel = this.getMasteryCheckPointLevel(this._masteryPoolXP);
+        // copy by SkillWithMastery.addMasteryPoolXP
+        const oldXP = this._masteryPoolXP.get(realm);
+        // const xpCap = this.getMasteryPoolCap(realm);
+        const newXP = Math.min(oldXP + xp, xpCap);
+        this._masteryPoolXP.set(realm, newXP);
+        const oldBonusLevel = this.getActiveMasteryPoolBonusCount(realm, oldXP);
+        const newBonusLevel = this.getActiveMasteryPoolBonusCount(realm, newXP);
         if (oldBonusLevel !== newBonusLevel) {
-            this.onMasteryPoolBonusChange(oldBonusLevel, newBonusLevel);
+          this.onMasteryPoolBonusChange(realm, oldBonusLevel, newBonusLevel);
         }
-    });
+        this.renderQueue.masteryPool.add(realm);
+    })
 
-    ctx.patch(Bank, "claimItemOnClick").replace(function (o, item, quantity) {
-        if (item.modifiers.masteryToken != undefined) {
-            const skill = item.modifiers.masteryToken[0].skill;
-            if ((skill instanceof SkillWithMastery) && skill.hasMastery) {
-                // If overflow, abort.
-                if (skill.masteryPoolXP >= skill.masteryPoolCap) { // like source code, here using 'masteryPoolCap'
-                    notifyPlayer(skill, "Abort! It's overflow.", "danger");
-                    return;
-                }
+    ctx.patch(Bank, "claimMasteryTokenOnClick").replace(function (o, item, quantity) {
+        const skill = item.skill;
+
+        if (skill.hasMastery) {
+            // If overflow, abort.
+            if (skill.getMasteryPoolXP(item.realm) >= skill.getMasteryPoolCap(item.realm)) { // like source code, here using 'masteryPoolCap'
+                notifyPlayer(skill, "Abort! It's overflow.", "danger", 0);
+                return;
             }
         }
 
         o(item, quantity);
     });
 
-    // add percent in spend mastery pool
-    ctx.patch(SpendMasteryMenuItem, 'updateProgress').after(function(returnValue, skill, action, spendAmount) {
-        if (!generalSettings.get('show-percent')) {
-            return;
-        }
-        const progress = skill.getMasteryProgress(action);
-        if (progress.level < 99) {
-            const nextLevel = Math.min(progress.level + spendAmount, 99);
-            const xpRequired = exp.level_to_xp(nextLevel) - progress.xp + 1;
-            const percent = (100 * xpRequired / skill.baseMasteryPoolCap).toFixed(2);
-            this.xpRequired.textContent += ` (${percent}%)`
-        }
-    });
+    // // add percent in spend mastery pool
+    // ctx.patch(SpendMasteryMenuItem, 'updateProgress').after(function(returnValue, skill, action, spendAmount) {
+    //     if (!generalSettings.get('show-percent')) {
+    //         return;
+    //     }
+    //     const progress = skill.getMasteryProgress(action);
+    //     if (progress.level < 99) {
+    //         const nextLevel = Math.min(progress.level + spendAmount, 99);
+    //         const xpRequired = exp.level_to_xp(nextLevel) - progress.xp + 1;
+    //         const percent = (100 * xpRequired / skill.baseMasteryPoolCap).toFixed(2);
+    //         this.xpRequired.textContent += ` (${percent}%)`
+    //     }
+    // });
 }
