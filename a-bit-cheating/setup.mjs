ba8328eda6c030e2c0fad3b2455945b9b1f460d7
SaulLawliet <october.sunbathe@gmail.com>
2024-06-11 12:30:44 +0800

    a-bit-cheating: Fixed for v1.3

--- a-bit-cheating/setup.mjs
+++ a-bit-cheating/setup.mjs
@@ -10,30 +10,11 @@ export function setup(ctx) {
         default: true
     });
 
-    // generalSettings.add({
-    //     type: 'switch',
-    //     name: 'agility-cost-reductions',
-    //     label: 'Agility Cost Reductions Are Capped at 100%. (Need Reload)',
-    //     hint: '95% => 100%',
-    //     default: true
-    // });
-
     ctx.onCharacterLoaded(ctx => {
-        // copy from: SlayerTask.getMonsterSelection
-        const canSlayer = (tier, monster) => {
-            const data = SlayerTask.data[tier];
-            const combatLevel = monster.combatLevel;
-            const monsterArea = game.getMonsterArea(monster);
-            let slayerLevelReq = 0;
-            if (monsterArea instanceof SlayerArea)
-                slayerLevelReq = monsterArea.slayerLevelRequired;
-            return (monster.canSlayer && combatLevel >= data.minLevel && combatLevel <= data.maxLevel);
-        }
-
         ctx.patch(CombatManager, 'spawnEnemy').after(function () {
             if (generalSettings.get('slayer-follow')) {
                 if (this.selectedArea && !(this.selectedArea instanceof Dungeon) && this.enemy.monster != this.slayerTask.monster) {
-                    if (canSlayer(this.slayerTask.tier, this.enemy.monster)) {
+                    if (this.slayerTask.getMonsterSelection(this.slayerTask.category).includes(this.enemy.monster)) {
                         this.slayerTask.monster = this.enemy.monster;
                         this.slayerTask.renderRequired = true;
                         this.slayerTask.render();
